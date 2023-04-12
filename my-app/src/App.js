8b813c2b78003a9d816fbe278ee627ca62923a07
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-12 01:07:01 +0000

    First component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
+import Expenses from "./components/Expenses";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses></Expenses>
     </div>
   );
 }
---
+++ my-app/src/components/Expenses.js
@@ -0,0 +1,5 @@
+function Expenses() {
+  return <h2>Expense Item!</h2>;
+}
+
+export default Expenses;
