34fc3dcbbc75d7e6a3f843131e95929d628f2bcd
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-12 01:38:17 +0000

    Adding classes-CSS

--- my-app/src/components/Expenses.js
+++ my-app/src/components/Expenses.js
@@ -2,11 +2,11 @@ import "./Expenses.css";
 
 function Expenses() {
   return (
-    <div className="">
+    <div className="expense-item">
       <div>March 28th 2021</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car insurance</h2>
-        <div>$300</div>
+        <div className="expense-item__price">$300</div>
       </div>
     </div>
   );
