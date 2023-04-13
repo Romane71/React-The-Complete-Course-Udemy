5790055a01697956b85b80124e419c77d8a85a39
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-13 02:00:41 +0000

    Adding Form Inputs

---
+++ my-app/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,25 @@
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls"></div>
+      <div className="new-expense__control">
+        <label>Title</label>
+        <input type="text" />
+      </div>
+      <div className="new-expense__control">
+        <label>Amount</label>
+        <input type="number" min="0.01" step="0.01" />
+      </div>
+      <div className="new-expense__control">
+        <label>Date</label>
+        <input type="date" min="2019-01-01" step="2012-12-31" />
+      </div>
+      <div className="new-expense__actions"></div>
+      <button type="Submit" Add Expense></button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
