3f3d5f478e4ee736875bbd9daca96c949452554e
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-15 03:28:02 +0000

    Wrap up-final steps of the expenses tracker

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import Card from "../Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -23,7 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
